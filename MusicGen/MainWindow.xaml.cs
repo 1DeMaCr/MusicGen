2e82e9a6c73e85f784739d91eb54f59bbafbe2fd
Demacr <serge2101@bk.ru>
2021-09-17 15:00:33 +0300

    Добавьте файлы проекта.

---
+++ MusicGen/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string notes;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int prevNote = 4;
+            int curNote;
+            int numOfNotes = 10;
+            Notes noteGen = new Notes();
+            notes = noteGen.NumToNote(prevNote);
+            for(int i = 0; i < numOfNotes; i++)
+            {
+                curNote = noteGen.NoteGen(prevNote);
+                notes = notes + " " + noteGen.NumToNote(curNote);
+                prevNote = curNote;
+            }
+
+            MessageBox.Show(notes);
+
+        }
+    }
+}
---
+++ MusicGen/Notes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicGen
+{
+    class Notes
+    {
+        public string NumToNote(int n)
+        {
+            string note="None";
+            switch (n)
+            {
+                case 1: note = "C"; break;
+                case 2: note = "C#"; break;
+                case 3: note = "D"; break;
+                case 4: note = "D#"; break;
+                case 5: note = "E"; break;
+                case 6: note = "F"; break;
+                case 7: note = "F#"; break;
+                case 8: note = "G"; break;
+                case 9: note = "G#"; break;
+                case 10: note = "A"; break;
+                case 11: note = "A#"; break;
+                case 12: note = "B"; break;
+            }
+            return note;
+        }
+
+        public int NoteGen(int prevNote)
+        {
+            int note;
+            var rand = new Random();
+            do
+                note = rand.Next(1, 15);
+            while (NoteIncorrect(prevNote, note));
+            
+            return note;
+        }
+
+        public bool NoteIncorrect(int prevNote, int curNote)
+        {
+            if (prevNote == curNote)
+            {
+                return false;
+            }
+            else if (prevNote > curNote)
+            {
+                return false;
+            }
+            else if (curNote % 2 == 0) return false;
+            else return true;
+        }
+    }
+}
